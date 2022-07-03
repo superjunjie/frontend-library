a000023e02e0b642f4e6da1829501a567fc117bb
YANJUNJIE <pcxiaoyan@gmail.com>
2022-07-03 15:47:16 +0800

    chore: add jest

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import Rate from '../components/Rate.vue'
 const count = ref(0)
 const color = ref('red')
+const value = ref(4)
 function add() {
   count.value++
   color.value = Math.random() > 0.5 ? 'blue' : 'red'
@@ -14,7 +15,7 @@ function add() {
   <h1 @click="add">
     {{ count }}
   </h1>
-  <Rate value="4" />
+  <Rate :value="value" />
 </template>
 
 <style scoped>
